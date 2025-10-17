a15e41ec6697637e9b4c279988029de02fd7c15a
Raul-B-Carrera <carreraraulbarbosa@gmail.com>
2025-10-16 21:51:39 -0300

    Add files via upload

---
+++ laco_while/Numero100.java
@@ -0,0 +1,14 @@
+package atividades;
+
+public class Numero100 {
+	public static void main(String[] args) {
+		int i;
+		
+		i = 1;
+		while(i<=100) {
+			System.out.println(i);
+			i=i+1;
+		}
+		
+	}
+}
